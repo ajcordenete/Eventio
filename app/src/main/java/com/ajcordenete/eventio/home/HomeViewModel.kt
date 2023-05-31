214dde1ad5ba1b3529360c76bea81d71c00f8a3b
ajcordenete <aljon.cordenete@gmail.com>
2023-05-31 16:01:14 +0800

    Add UI for home dashboard

--- app/src/main/java/com/ajcordenete/eventio/home/HomeViewModel.kt
+++ app/src/main/java/com/ajcordenete/eventio/home/HomeViewModel.kt
@@ -2,12 +2,13 @@ package com.ajcordenete.eventio.home
 
 import android.os.Bundle
 import com.ajcordenete.core.base.BaseViewModel
+import com.ajcordenete.data.feature.event.EventRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-
+    private val eventRepository: EventRepository
 ): BaseViewModel() {
 
     override fun isFirstTimeUiCreate(bundle: Bundle?) {}
